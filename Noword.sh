7bd180d5ba70d5abc1160fe2c6795e84ed2cc4d1
manjunathanaatmanni <manjunathankrishnan@gmail.com>
2022-06-22 07:12:48 +0000

    Number in words

---
+++ Noword.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+echo -n "Enter number : "
+read n
+for (( i=1; i<=$n; i++ ))
+do
+    Z=$(echo $n | cut -c $i)
+    case $Z in
+        0) echo -n "zero " ;;
+        1) echo -n "one " ;;
+        2) echo -n "two " ;;
+        3) echo -n "three " ;;
+        4) echo -n "four " ;;
+        5) echo -n "five " ;;
+        6) echo -n "six " ;;
+        7) echo -n "seven " ;;
+        8) echo -n "eight " ;;
+        9) echo -n "nine " ;;
+    esac
+done
+echo "in word"
