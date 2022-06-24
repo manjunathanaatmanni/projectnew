9b2cbc89edd75a5437b4b39230aa5992bfef9ca4
manjunathanaatmanni <manjunathankrishnan@gmail.com>
2022-06-24 06:57:26 +0000

    Updated renamefile

--- renamefile.sh
+++ renamefile.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
-read -p "Enter the sh filename: " original
-read -p "Enter the ext filename: " rename
-$(mv $original $rename)
-echo "The file are renamed."
+#read -p "Enter the sh filename: " original
+#read -p "Enter the ext filename: " rename
+#$(mv $original $rename)
+#echo "The file are renamed."
+read -p "Enter the sh file: " file
+#read -p "Enter the ext file: " rename
+if [ -f $file ]
+then
+	a=`ls $file | cut -d "." -f1`
+        echo `mv $file  $a.exe`	
+ fi
 
+ #for i in *
+#do
+#	mv *.sh *.exe
+#done
